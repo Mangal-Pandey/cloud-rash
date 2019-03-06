fde20add03469065c6820bc82b8c876319c17743
Mangal-Pandey <amitvm97@gmail.com>
2019-03-06 20:33:31 +0530

    initial commit

---
+++ Cloud Rash/Assets/scripts/BikeControllers/BikeParentControllerVeqUplAT.cs
@@ -0,0 +1,80 @@
+﻿
+//Trying to add inertia of motion
+/*
+Motion parameters that'll vary with bike models:
+1. accelerating power
+2. braking power
+3. turning radius (turning angle)
+*/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BikeParentControllerVeqUplAT: MonoBehaviour {
+    public float acceleratingPower, brakingPower;
+    public float currentForwardSpeed, currentAccel;
+
+    Rigidbody rb;
+
+    bool accelerating, braking, turningRight, turningLeft;
+    Vector3 currentVelocity, forward;
+    float speedToTurnAngleMultiplier;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+
+        acceleratingPower = 50f;
+        brakingPower = 40f;
+        currentAccel = 0f;
+        speedToTurnAngleMultiplier = 0.02f;
+
+	}
+
+	void Update () {
+        accelerating = Input.GetKey(KeyCode.UpArrow);
+        braking = Input.GetKey(KeyCode.DownArrow);
+        turningRight = Input.GetKey(KeyCode.RightArrow);
+        turningLeft = Input.GetKey(KeyCode.LeftArrow);
+
+        forward = transform.forward.normalized;
+        currentVelocity = rb.velocity;
+        currentForwardSpeed = Vector3.Dot(currentVelocity, forward);
+
+
+        if (accelerating)
+        {
+            //gradually increase forward velocity
+            currentAccel = Mathf.Min(currentAccel + 0.5f, acceleratingPower);
+        }
+        else if (currentForwardSpeed > 0.8)
+        {
+            //gradually decrease forward velocity
+            currentAccel = Mathf.Max(currentAccel - 0.1f, 0f);
+        }
+
+        if (braking)
+        {
+            //rapidly decrease forward velocity
+            currentAccel = Mathf.Max(currentAccel - 0.5f, 0f);
+        }
+
+        rb.AddForce(forward * currentAccel, ForceMode.VelocityChange);
+
+        if(currentForwardSpeed > 0.8) { 
+            if (turningRight){
+                //rotate bike to the right, gradually
+                transform.RotateAround(transform.position, transform.up, currentForwardSpeed * speedToTurnAngleMultiplier);
+            }
+
+            if (turningLeft){
+                //rotate bike to he left, gradually
+                transform.RotateAround(transform.position, transform.up, -currentForwardSpeed * speedToTurnAngleMultiplier);
+            }
+        }
+
+    }
+
+
+}
---
+++ Cloud Rash/Assets/scripts/BikeControllers/ParentedBikeController.cs
@@ -0,0 +1,69 @@
+﻿//WORKING COPY. Every latest version updated to BikeController.cs
+
+
+//Trying to add inertia of motion
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParentedBikeController : MonoBehaviour {
+
+
+	public int animationTurnParam = 1;
+	public int turnCurrentStat;
+	Animator animator;
+
+
+	//values for turnedCurrentStat
+	const int ISSTRAIGHT = 1;
+	const int TURNEDRIGHT = 2;
+	const int TURNEDLEFT = 3;
+
+	void Start () {
+		animator = GetComponent<Animator> ();
+		turnCurrentStat = ISSTRAIGHT;
+	}
+
+	void Update () {
+
+		bool turningRight = Input.GetKey (KeyCode.RightArrow);
+		bool turningLeft = Input.GetKey (KeyCode.LeftArrow);
+
+		if (turningRight) {
+
+			if (turnCurrentStat == ISSTRAIGHT)
+				animationTurnParam = 2;
+			else if (turnCurrentStat == TURNEDLEFT)
+				animationTurnParam = 2;
+
+
+			turnCurrentStat = TURNEDRIGHT;
+		}
+
+		if (turningLeft) {
+
+			if (turnCurrentStat == ISSTRAIGHT)
+				animationTurnParam = 5;
+			else if (turnCurrentStat == TURNEDRIGHT)
+				animationTurnParam = 5; 
+
+			turnCurrentStat = TURNEDLEFT;
+		}
+
+		if ((!turningLeft && !turningRight) /*|| !moving*/) {
+
+			if (turnCurrentStat == TURNEDRIGHT)
+				animationTurnParam = 3;
+			else if (turnCurrentStat == TURNEDLEFT)
+				animationTurnParam = 1; 
+
+			turnCurrentStat = ISSTRAIGHT;
+		}
+
+		animator.SetInteger("bikeAnimParam", animationTurnParam);
+	}
+
+
+}
